c4706f55d96438bfbbf7e0d18ae7e3df971d5228
sandeep18p <sandeep.pansari98@gmail.com>
2024-04-29 18:30:59 +0530

    three

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
+
+////
 const http = require("http");
+const fs = require("fs");
 const port = 3000;
 
-// TODO: Create an HTTP server
+const filePath = process.argv[2];
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World!\n");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error reading file");
+      return;
+    }
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello, World!\n");
+  });
 });
 
-// TODO: Listen on port 3000
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
 
 // DO NOT EDIT BELOW THIS LINE (Uncomment it once you are done with your code)
