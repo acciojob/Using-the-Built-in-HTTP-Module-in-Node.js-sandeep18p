df05839cff70421b32fad49fc568c93566ed8d66
sandeep18p <sandeep.pansari98@gmail.com>
2024-04-29 18:24:15 +0530

    one

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
 const http = require('http');
+const fs = require('fs');
+
+
+if (process.argv.length < 3) {
+  console.error('Please provide a file path as an argument.');
+  process.exit(1);
+}
+
+
+const filePath = process.argv[2];
+
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err.message);
+    process.exit(1);
+  }
+ 
+  
+  console.log('File contents:', data);
+});
+
 
 // TODO: Create an HTTP server
 
 // TODO: Listen on port 3000
 
+
+//create a server object:
+http.createServer(function (req, res) {
+  
+  res.statusCode = 200;
+ 
+  res.end("Hello, World!\n");
+}).listen(3000); 
 // DO NOT EDIT BELOW THIS LINE (Uncomment it once you are done with your code)
 
