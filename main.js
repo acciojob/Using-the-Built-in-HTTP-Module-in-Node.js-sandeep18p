9c3a96054073678abd1c60a71bd532447812efa9
sandeep18p <sandeep.pansari98@gmail.com>
2024-04-29 18:32:14 +0530

    fou

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
-
-////
 const http = require("http");
-const fs = require("fs");
 const port = 3000;
 
-const filePath = process.argv[2];
-
+// TODO: Create an HTTP server
 const server = http.createServer((req, res) => {
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading file:", err);
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Error reading file");
-      return;
-    }
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello, World!\n");
-  });
+
+    
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!\n");
 });
 
+// TODO: Listen on port 3000
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
